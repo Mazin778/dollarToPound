4c3831d3974173ac9810298d50faffb81af6f4bb
Mazin Ahmos AL-tarek Awad <62282787+Mazin778@users.noreply.github.com>
2022-08-01 19:48:17 +0200

    fristcommit1

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 let pound = document.getElementById("pound");
 let dollar = document.getElementById("dollar");
+let bbtn = document.getElementById("scbtn");
 
 dollar.onkeyup = () => {
     pound.value = dollar.value * 18.98;
 }
 pound.onkeyup = () => {
     dollar.value = pound.value / 18.98;
+}
+window.onscroll = function() {
+    if (scrollY >= 100) {
+        bbtn.style.display = "block";
+    } else {
+        bbtn.style.display = "none";
+    }
+}
+bbtn.onclick = function() {
+    scroll({
+        top: 0,
+        behavior: "smooth",
+    })
 }
